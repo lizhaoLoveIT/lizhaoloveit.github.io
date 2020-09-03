6dc5335089fe786764ec466c84d41d3ef7190cf9
Ammar <lacuself@126.com>
2020-09-03 23:29:38 +0800

    Site updated: 2020-09-03 23:29:38

--- js/nightshift.js
+++ js/nightshift.js
@@ -13,13 +13,13 @@ $(function () {
   function changeReadModel() {
     if (Cookies.get("night-mode") == "night") {
       $("body").addClass("night-mode");
-      $("#nightshift").removeClass("fa-moon-o").addClass("fa-sun-o");
+      $("#nightshift").removeClass("fas fa-moon").addClass("far fa-sun");
     }
 
     // 非夜間模式
     if (Cookies.get("night-mode") == "day") {
       $("body").removeClass("night-mode");
-      $("#nightshift").removeClass("fa-sun-o").addClass("fa-moon-o");
+      $("#nightshift").removeClass("far fa-sun").addClass("fas fa-moon");
     }
   }
 
